8374958e940e282a57837e8278f5afc52857013b
Mubashir M.Ibrahim <mubashiribrahim@cispl.com>
2024-09-02 09:46:19 +0500

    nav-top-sticky

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -11,7 +11,7 @@ export function CustomNavbar({ onSearch }) {
   };
 
   return (
-    <Navbar fluid={true} rounded={true} className="container mx-auto px-[1.25rem]">
+    <Navbar fluid={true} rounded={true} className="container mx-auto px-[1.25rem] sticky top-0 py-3">
       <Navbar.Brand href="/">
         <img src="https://flowbite-react.com/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
